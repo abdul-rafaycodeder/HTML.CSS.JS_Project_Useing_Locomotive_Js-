ff948bf6070bcc52da5262ab2956389fb510f268
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-05-29 00:59:51 +0500

    coding

--- script.js
+++ script.js
@@ -15,4 +15,32 @@ observer.observe(videoSection);
 //     smooth: true
 // });
 
-let a = document.getElementById('elem1')
+
+var elemC = document.querySelector("#elem-container")
+var fixImg = document.querySelector("#fixed-image")
+elemC.addEventListener('mouseenter', () => {
+    fixImg.style.display = 'block'
+})
+elemC.addEventListener('mouseleave', () => {
+    fixImg.style.display = 'none'
+})
+
+
+// var elem1 = document.querySelector("#elem1")
+// elem1.addEventListener("mouseenter", () => {
+//     var image = elem1.getAttribute("data-img")
+//     fixImg.style.backgroundImage = `url(${image})`
+
+// })
+
+
+var elem = document.querySelectorAll(".elem")
+elem.forEach(function (e) {
+
+    e.addEventListener("mouseenter", () =>{
+        var image = e.getAttribute("data-img")
+        fixImg.style.backgroundImage = `url(${image})`
+    })
+
+})
+
---
+++ text.js
@@ -0,0 +1,4 @@
+var a = document.querySelector('img')
+console.log(a)
+
+console
